44d64c3291a873510b07e74d216c29ac9819c8d1
Abbas Rizvi <aarizv@gmail.com>
2020-10-12 23:17:19 -0400

    added skeleton function

---
+++ is_leap_year.R
@@ -0,0 +1,19 @@
+#' @param year numeric vector 
+#' 
+#' @return logical vector
+#' @example is_leap_year(c(1996, 1997))
+#' @export
+
+is_leap_year <- function(year){
+    # body function 
+    condition1 <- year %% 4 == 0
+    condition2 <- year %% 100 != 0
+    condition3 <- year %% 400 == 0 
+    logical_answer <- condition1 & (condition2 | condition3)
+    return(logical_answer)
+    
+}
+
+
+
+
