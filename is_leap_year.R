0b91d3e94b1dd06fee6712678ead4341aac853aa
Abbas Rizvi <aarizv@gmail.com>
2020-10-12 23:35:09 -0400

    added title

--- is_leap_year.R
+++ is_leap_year.R
@@ -1,19 +1,16 @@
-#' @param year numeric vector 
-#' 
+#' Function that determines if a year is a leap year or not
+#' @param year numeric vector
+#'
 #' @return logical vector
 #' @example is_leap_year(c(1996, 1997))
 #' @export
 
 is_leap_year <- function(year){
-    # body function 
+    # body function
     condition1 <- year %% 4 == 0
     condition2 <- year %% 100 != 0
-    condition3 <- year %% 400 == 0 
+    condition3 <- year %% 400 == 0
     logical_answer <- condition1 & (condition2 | condition3)
     return(logical_answer)
-    
+
 }
-
-
-
-
